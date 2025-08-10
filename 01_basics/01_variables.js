291157f33dce0c27737acc7c861d4122caf96a4a
Kanak Kumari <kanak8694@gmail.com>
2025-08-10 05:11:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="kanak8694@gmail"
+var accountPassword="12345"
+accountCity="ranchi"
+let accountState;
+// accountId=2 not allowed
+accountEmail="shfjefb"
+accountPassword="44443"
+accountCity="delhi"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+/*
+prefer not to use var bcoz of issue in block 
+scope and functional scope
+*/
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as new version
+// alert(3+3) we are using node js not browser
+console.log(3
+    +3)//code readibility should be high
+
+console.log("kanak")
+let state;
+let name ="kanak"
+let age=66
+let isLoggedIn=false
+//number 2 to power of 53
+//bigint
+//string=""
+//boolean=true/false
+//null=stand alone value
+//undefined=value is not assigned
+//symbol=for uniqueness
+//objects
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
